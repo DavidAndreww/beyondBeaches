921fda75e84075f38d8a922c83b9ba27e5fde13f
Giles Smith <gilesomofa@yahoo.com>
2022-01-04 00:59:53 -0400

    online directory

--- like_button.js
+++ like_button.js
@@ -1,3 +1,7 @@
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import React, {Component} from 'react';
+// import Card from 'react-bootstrap/Card';
+
 'use strict';
 
 const e = React.createElement;
@@ -5,16 +9,7 @@ const e = React.createElement;
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = [ {
-        venueName: '',
-        venueLocation: '',
-        venuePhoneNumber:'',
-        venueDescription: '',
-        
-        
-
-    },
-];
+    this.state = { liked: false };
   }
 
   render() {
@@ -29,5 +24,54 @@ class LikeButton extends React.Component {
     );
   }
 }
+
+// const displayCard = React.createElement;
+
+// class onlineDirectory extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = [
+// 			{
+// 				venueName: 'Barefoot Buddha',
+// 				venueLocation: 'Havensight',
+// 				venuePhoneNumber: '+1 (340)777-3668',
+// 				venueDescription: 'Cafe, Gift Shop',
+// 				venueBio: '',
+// 				venueReviews: '',
+// 				venueComments: '',
+// 			},
+// 			{
+// 				venueName: 'Stir It Up',
+// 				venueLocation: 'Havensight',
+// 				venuePhoneNumber: '+1 (340)777-3668',
+// 				venueDescription: 'Cafe',
+// 				venueBio:
+// 					'Small vibrant cafe that serves coffees, teas, kombucha, and a wonderful variety of scratch made pastries.',
+// 				venueReviews: '',
+// 				venueComments: '',
+// 				venueSchedule: [{ open: '' }, { close: '' }],
+// 			},
+// 			{
+// 				venueName: 'Doghouse Pub',
+// 				venueLocation: 'Havensight',
+// 				venuePhoneNumber: '+1 (340)777-3668',
+// 				venueDescription: 'Bar, Dine',
+// 				venueBio: '',
+// 				venueReviews: '',
+// 				venueComments: '',
+// 				venueSchedule: [{ open: '' }, { close: '' }],
+// 			},
+// 			{
+// 				venueName: 'Deli Deck',
+// 				venueLocation: 'Havensight',
+// 				venuePhoneNumber: '+1 (340)777-3668',
+// 				venueDescription: 'Restaurant',
+// 				venueBio: '',
+// 				venueReviews: '',
+// 				venueSchedule: [{ open: '' }, { close: '' }],
+// 			},
+// 		];
+// 	}
+
 const domContainer = document.querySelector('#like_button_container');
 ReactDOM.render(e(LikeButton), domContainer);

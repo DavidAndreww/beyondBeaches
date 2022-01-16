b8a9df33ca21e382ee214483efe0bbc9e6dc844c
Giles Smith <gilesomofa@yahoo.com>
2022-01-16 02:03:10 -0400

    cards

--- cards.js
+++ cards.js
@@ -1,23 +1,21 @@
 //I need to loop venues array and return a card for each object. 
 //this new array of cards needs to be pushed to a container in the dom as innerHTML 
-var createCard = (venues)=>{
-	if (venues.venueId){console.log('hello');
-	}
-}
-document.getElementByClass('card').innerHTML = venueName
-
+// var createCard = (venues)=>{
+// 	if (venues.venueId){console.log('hello');
+// 	}
+// }
+// document.getElementByClass('card').innerHTML = venueName
+const body = document.querySelector('body')
+console.log(body);
 let venues = [
 	{
 		venueName: 'Barefoot Buddha',
 		venueId: 0,
 		venueLocation: 'Havensight',
 		venuePhoneNumber: '+1 (340)777-3668',
-		venueDescription: 'Cafe, Gift Shop',
+		venueCategory: 'EAT/DRINK/<BR>GIFTS',
 		venueBio: '',
-		venueEmail: '',
         venueWebsiteUrl:'',
-		venueReviews: '',
-		venueComments: '',
 		venueSchedule: [{ open: '' }, { close: '' }],
 	},
 	{
@@ -25,13 +23,10 @@ let venues = [
 		venueId: 1,
 		venueLocation: 'Havensight',
 		venuePhoneNumber: '+1 (340)777-3668',
-		venueDescription: 'Cafe',
+		venueCategory: 'EAT/DRINK',
 		venueBio:
 		'Small vibrant cafe that serves coffees, teas, kombucha, and a wonderful variety of scratch made pastries.',
-        venueEmail: '',
         venueWebsiteUrl:'',
-		venueReviews: '',
-		venueComments: '',
 		venueSchedule: [{ open: '' }, { close: '' }],
 	},
 	{
@@ -39,12 +34,9 @@ let venues = [
 		venueId: 2,
 		venueLocation: 'Havensight',
 		venuePhoneNumber: '+1 (340)777-3668',
-		venueDescription: 'Bar, Dine',
+		venueCategory: 'EAT/DRINK',
 		venueBio: '',
-        venueEmail: '',
         venueWebsiteUrl:'',
-        venueReviews: '',
-		venueComments: '',
 		venueSchedule: [{ open: '' }, { close: '' }],
 	},
 	{
@@ -52,17 +44,25 @@ let venues = [
 		venueId: 3,
 		venueLocation: 'Havensight',
 		venuePhoneNumber: '+1 (340)777-3668',
-		venueDescription: 'Restaurant',
+		venueCategory: 'EAT/DRINK',
 		venueBio: '',
-        venueEmail: '',
-        venueWebsiteUrl:'',
-        venueReviews: '',
+		venueWebsiteUrl:'',
 		venueSchedule: [{ open: '' }, { close: '' }],
 	},
 ];
 
 
 
-document.getElementById('cards_container').innerHTML = venues.map(createCard,(venue) => {
-console.log('i am connected')
+  venues.forEach((venue) => {
+body.innerHTML += 
+	`<div id="card">
+	<h1 id="cardCategory">${venue.venueCategory}</h1>
+	<h2 id="venueName">${ venue.venueName }</h2>
+	<div id="venueInfo">
+		<p id="" class="venueHashtag"></p>
+		<p id="venueBio"></p>
+		<div id="venueCardLeft"></div>
+		<div id="venueCardRight"></div>
+	</div>	
+</div>`
 });
